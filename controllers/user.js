ab2de3bb5797bfbd1e3a2acd0d20b0c3c1bd8a04
Daniyal Alam <90774564+DaniyalAlam09@users.noreply.github.com>
2023-05-21 14:29:37 +0500

    Get Users APi

--- controllers/user.js
+++ controllers/user.js
@@ -43,3 +43,14 @@ exports.login = async (req, res) => {
     res.status(500).json({ message: 'Login failed' });
   }
 }
+
+
+exports.viewUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { createUser, login } = require("../../controllers/user");
+const { createUser, login, viewUsers } = require("../../controllers/user");
 
 router.post("/register", createUser);
 router.post("/login", login);
+router.get("/allusers", viewUsers);
 
 module.exports = router;
